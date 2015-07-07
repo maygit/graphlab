c8541820fb1edd81dc372c46ac5ed732575382c2
mzj <mzj@node248.(none)>
2015-07-07 11:58:36 +0800

    correct to the right version of WCC

--- apps/a_concomp/a_concomp.cpp
+++ apps/a_concomp/a_concomp.cpp
@@ -1,5 +1,5 @@
-/*  
- * Copyright (c) 2009 Carnegie Mellon University. 
+/*
+ * Copyright (c) 2009 Carnegie Mellon University.
  *     All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,175 +14,197 @@
  *  express or implied.  See the License for the specific language
  *  governing permissions and limitations under the License.
  *
+ * For more about this software visit:
+ *
+ *      http://www.graphlab.ml.cmu.edu
  *
  */
 
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <boost/unordered_map.hpp>
+#include <time.h>
 
 #include <graphlab.hpp>
+#include <graphlab/graph/distributed_graph.hpp>
 
-// The vertex data is just the id value
-typedef graphlab::vertex_id_type vertex_data_type;
-typedef graphlab::vertex_id_type edge_data_type;
-
-typedef double gather_type;
-
-// The graph type is determined by the vertex and edge data types
-typedef graphlab::distributed_graph<vertex_data_type, edge_data_type> graph_type;
-
-/*
- * A simple function used by graph.transform_vertices(init_vertex);
- * to initialize the vertex data to the vertex id
- */
-void init_vertex(graph_type::vertex_type& vertex) { vertex.data() = -1; }
-
-struct min_combiner : public graphlab::IS_POD_TYPE {
-  vertex_data_type value;
-
-  min_combiner() {
-    value = -1;
+struct vdata {
+  uint64_t labelid;
+  vdata() :
+      labelid(0) {
   }
-    
-  min_combiner& operator+=(const min_combiner& other) { 
-    if (other.value < value) {
-        value = other.value;
-    }
-    return *this; 
+
+  void save(graphlab::oarchive& oarc) const {
+    oarc << labelid;
+  }
+  void load(graphlab::iarchive& iarc) {
+    iarc >> labelid;
   }
 };
 
-class concomp :
-  public graphlab::ivertex_program<graph_type, gather_type, min_combiner>,
-  public graphlab::IS_POD_TYPE {
+typedef graphlab::distributed_graph<vdata, graphlab::empty> graph_type;
 
-  // set changed to determine which edges to scatter on
-  bool changed;
+//set label id at vertex id
+void initialize_vertex(graph_type::vertex_type& v) {
+  v.data().labelid = v.id();
+}
 
-  // local copy of the message value
-  vertex_data_type message_value; 
-public:
-  // Receive inbound message (minimum data of adjacent vertices)
-  void init(icontext_type& context, const vertex_type& vertex, const message_type& message) {
-    // message.value == 4294967295 on first run, so init message_value to vertex data.
-    if (message.value == 4294967295) {
-      message_value = vertex.id();
-    } else {
-      // else, set the local copy to the message parameter.
-      message_value = message.value;
-    }
-
+//message where summation means minimum
+struct min_message {
+  uint64_t value;
+  explicit min_message(uint64_t v) :
+      value(v) {
+  }
+  min_message() :
+      value(std::numeric_limits<uint64_t>::max()) {
+  }
+  min_message& operator+=(const min_message& other) {
+    value = std::min<uint64_t>(value, other.value);
+    return *this;
   }
 
-  edge_dir_type gather_edges(icontext_type& context, const vertex_type& vertex) const {
+  void save(graphlab::oarchive& oarc) const {
+    oarc << value;
+  }
+  void load(graphlab::iarchive& iarc) {
+    iarc >> value;
+  }
+};
+
+class label_propagation: public graphlab::ivertex_program<graph_type, size_t,
+    min_message>, public graphlab::IS_POD_TYPE {
+private:
+  size_t recieved_labelid;
+  bool perform_scatter;
+public:
+  label_propagation() {
+    recieved_labelid = std::numeric_limits<size_t>::max();
+    perform_scatter = false;
+  }
+
+  //receive messages
+  void init(icontext_type& context, const vertex_type& vertex,
+      const message_type& msg) {
+    recieved_labelid = msg.value;
+  }
+
+  //do not gather
+  edge_dir_type gather_edges(icontext_type& context,
+      const vertex_type& vertex) const {
     return graphlab::NO_EDGES;
   }
+  size_t gather(icontext_type& context, const vertex_type& vertex,
+      edge_type& edge) const {
+    return 0;
+  }
 
-  // Change the vertex data if any of its neighbors have a lower data value.
-  void apply(icontext_type& context, vertex_type& vertex, const gather_type& total) {
-    // mark if values differ to determine which edges to scatter on.
-    if (message_value < vertex.data()) {
-        changed = true;
-        vertex.data() = message_value;
-    } else {
-        changed = false;
+  //update label id. If updated, scatter messages
+  void apply(icontext_type& context, vertex_type& vertex,
+      const gather_type& total) {
+    if (recieved_labelid == std::numeric_limits<size_t>::max()) {
+      perform_scatter = true;
+    } else if (vertex.data().labelid > recieved_labelid) {
+      perform_scatter = true;
+      vertex.data().labelid = recieved_labelid;
     }
   }
 
-  edge_dir_type scatter_edges(icontext_type& context, const vertex_type& vertex) const {
-    // If the vertex data changed, scatter along all edges. Otherwise stop.
-    if (changed) {
-        return graphlab::ALL_EDGES;
-    } else {
-        return graphlab::NO_EDGES;
-    }
+  edge_dir_type scatter_edges(icontext_type& context,
+      const vertex_type& vertex) const {
+    if (perform_scatter)
+      return graphlab::ALL_EDGES;
+    else
+      return graphlab::NO_EDGES;
   }
 
-  // Scatter to scatter_edges edges with the new message value.
-  void scatter(icontext_type& context, const vertex_type& vertex, edge_type& edge) const {
-    bool isEdgeSource = (vertex.id() == edge.source().id());
-    bool hasSameData = isEdgeSource ? (vertex.data() == edge.target().data()) : (vertex.data() == edge.source().data()) ;
-    if (!hasSameData) {
-      min_combiner combiner;
-      combiner.value = message_value;
-
-      context.signal(isEdgeSource ? edge.target() : edge.source(), combiner);
+  //If a neighbor vertex has a bigger label id, send a massage
+  void scatter(icontext_type& context, const vertex_type& vertex,
+      edge_type& edge) const {
+    if (edge.source().id() != vertex.id()
+        && edge.source().data().labelid > vertex.data().labelid) {
+      context.signal(edge.source(), min_message(vertex.data().labelid));
+    }
+    if (edge.target().id() != vertex.id()
+        && edge.target().data().labelid > vertex.data().labelid) {
+      context.signal(edge.target(), min_message(vertex.data().labelid));
     }
   }
 };
 
-/* We want to save the final graph so we define a write which will be
-* used in graph.save("path/prefix", concomp_writer()) to save the graph.
-*/
-struct concomp_writer {
+class graph_writer {
+public:
   std::string save_vertex(graph_type::vertex_type v) {
     std::stringstream strm;
-    strm << v.id() << "\t" << v.data() << "\n";
+    strm << v.id() << "," << v.data().labelid << "\n";
     return strm.str();
   }
-  std::string save_edge(graph_type::edge_type e) { return ""; }
-}; // end of concomp writer
-          
+  std::string save_edge(graph_type::edge_type e) {
+    return "";
+  }
+};
 
 int main(int argc, char** argv) {
-  // Initialize control plain using mpi
+  std::cout << "Connected Component\n\n";
+
   graphlab::mpi_tools::init(argc, argv);
   graphlab::distributed_control dc;
-  global_logger().set_log_level(LOG_INFO);
-  
-  // Parse command line options -----------------------------------------------
-  graphlab::command_line_options clopts("Connected Components algorithm.");
+  global_logger().set_log_level(LOG_DEBUG);
+  //parse options
+  graphlab::command_line_options clopts("Connected Component.");
   std::string graph_dir;
-  std::string format = "snap";
-  std::string execution_type = "asynchronous";
-  clopts.attach_option("graph", graph_dir, "The graph file. Required ");
-  clopts.add_positional("graph");
-  clopts.attach_option("format", format, "The graph file format");
-  clopts.attach_option("execution", execution_type, "Execution type (synchronous or asynchronous)");
-
   std::string saveprefix;
+  std::string format = "adj";
+  std::string exec_type = "asynchronous";
+  clopts.attach_option("graph", graph_dir,
+                       "The graph file. This is not optional");
+  clopts.add_positional("graph");
+  clopts.attach_option("format", format,
+                       "The graph file format");
+  clopts.attach_option("execution", exec_type,
+                       "use sync or async engine");
   clopts.attach_option("saveprefix", saveprefix,
-                       "If set, will save the resultant pagerank to a "
-                       "sequence of files with prefix saveprefix");
-
-  if(!clopts.parse(argc, argv)) {
+                       "If set, will save the pairs of a vertex id and "
+                       "a component id to a sequence of files with prefix "
+                       "saveprefix");
+  if (!clopts.parse(argc, argv)) {
     dc.cout() << "Error in parsing command line arguments." << std::endl;
     return EXIT_FAILURE;
   }
   if (graph_dir == "") {
-    dc.cout() << "Graph not specified. Cannot continue";
+    std::cout << "--graph is not optional\n";
     return EXIT_FAILURE;
   }
- 
-  // Build the graph ----------------------------------------------------------
+
   graph_type graph(dc, clopts);
+
+  //load graph
   dc.cout() << "Loading graph in format: "<< format << std::endl;
   graph.load_format(graph_dir, format);
-  // must call finalize before querying the graph
+  graphlab::timer ti;
   graph.finalize();
+  dc.cout() << "Finalization in " << ti.current_time() << std::endl;
+  graph.transform_vertices(initialize_vertex);
 
-  graph.transform_vertices(init_vertex);
-
-  dc.cout() << "#vertices: " << graph.num_vertices() << " #edges:" << graph.num_edges() << std::endl;
-
-  graphlab::omni_engine<concomp> engine(dc, graph, execution_type, clopts);
-
-  min_combiner initial_message;
-  initial_message.value = -1;
-
-  engine.signal_all(initial_message);
-
+  //running the engine
+  time_t start, end;
+  graphlab::omni_engine<label_propagation> engine(dc, graph, exec_type, clopts);
+  engine.signal_all();
+  time(&start);
   engine.start();
 
-  const float runtime = engine.elapsed_seconds();
-  dc.cout() << "Finished Running engine in " << runtime << " seconds." << std::endl;
-
-  if (saveprefix != "") {
-    graph.save(saveprefix, concomp_writer(),
-                false,    // do not gzip
-                true,     // save vertices
-                false);   // do not save edges
+  //write results
+  if (saveprefix.size() > 0) {
+    graph.save(saveprefix, graph_writer(),
+        false, //set to true if each output file is to be gzipped
+        true, //whether vertices are saved
+        false); //whether edges are saved
   }
 
   graphlab::mpi_tools::finalize();
 
   return EXIT_SUCCESS;
 }
+
---
+++ apps/a_concomp/merge.sh
@@ -0,0 +1,37 @@
+
+if [ $# -eq 0  ]; then
+echo "please input the path"
+exit -1
+fi
+
+str=gl_merge_"`date +%s`".txt
+#echo $str
+#echo $1
+cat $1 > ~/mzjdata/tmp_result.txt
+echo "Pagerank: write to tmp_result.txt"
+#cat ~/mzjdata/tmp_result.txt | sort -n -k 2 > ~/mzjdata/$str
+if [ $# -eq 2  ]; then
+str=$2"`date +%s`".txt
+#head -n 10 ~/mzjdata/gl_merge_"$str".txt
+cat ~/mzjdata/tmp_result.txt | sort -t"," -k2nr,2 > ~/mzjdata/$str
+head -n 10 ~/mzjdata/$str
+
+else
+echo "WCC: write to tmp file"
+str=$2"`date +%s`".txt
+#echo -e "\e[1;31m This is red text \e[0m"
+
+cat ~/mzjdata/tmp_result.txt | sort -t"," -k1n,1 > ~/mzjdata/$str
+echo -e "\e[0;32m total components count: \e[0m"
+
+#cat ~/mzjdata/$str | awk -F ',' '{print $2}' | uniq -c | wc -l
+cat ~/mzjdata/$str | sort -t"," -k2n,2 |uniq -c | wc -l
+
+topn=20
+echo -e "\e[0;32m top $topn components \e[0m"
+echo "size  comp_id"
+
+#cat ~/mzjdata/$str | awk -F ',' '{print $2}' | uniq -c | head -n $topn
+cat ~/mzjdata/$str | sort -t"," -k2n,2 |awk -F ',' '{print $2}' |uniq -c | head -n $topn
+fi
+rm ~/mzjdata/tmp_result.txt
